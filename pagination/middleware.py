2af78bbfd29a07431fe89f18fe7e31564175a1a3
Pavel Volkov <paxapy@gmail.com>
2012-06-26 21:48:59 +0400

    changing middleware to support clean url

--- pagination/middleware.py
+++ pagination/middleware.py
@@ -1,3 +1,8 @@
+import re
+from django.conf import settings
+URL_PATTERN = getattr(settings, 'PAGINATION_URL_PATTERN', r'(pages/)(?P<page>\d+)/$')
+CLEAN_URL = getattr(settings, 'PAGINATION_CLEAN_URL', False)
+
 def get_page(self):
     """
     A function which will be monkeypatched onto the request to get the current
@@ -13,5 +18,26 @@ class PaginationMiddleware(object):
     Inserts a variable representing the current page onto the request object if
     it exists in either **GET** or **POST** portions of the request.
     """
+    def __init__(self):
+        self.path = re.match(r'\(([\w/_\.]+)\)', URL_PATTERN).group(1)
+
     def process_request(self, request):
-        request.__class__.page = property(get_page)
+
+        if CLEAN_URL:
+            current_page = 1
+            separator = ''
+            page_path = self.path
+            if page_path == '/':
+                page_path = ''
+                separator = '/'
+            match = re.compile(URL_PATTERN).search(request.path)
+            if match is not None:
+                current_page = int(match.group('page'))
+                path = re.sub(URL_PATTERN, separator, request.path)
+                del request.path_info
+                request.__class__.path_info = path
+
+            request.__class__.page = current_page
+            request.__class__.page_path = page_path
+        else:
+            request.__class__.page = property(get_page)
